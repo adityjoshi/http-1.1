30837fd84ee2b327c8acecc7c0fdeaf46411bcbd
adi <aditya03joshi@gmail.com>
2025-12-08 13:43:17 +0530

    request

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,5 +1,42 @@
 package request
 
-func main() {
+import (
+	"io"
+	"strings"
+)
 
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+var Error_Bad_Request_Line = "malformed http"
+func parseRequestLine(b string) (*RequestLine, string ,error) {
+	idx = strings.Index(b, "\r\n")
+	if idx = -1 {
+		return nil,b,nil
+	}
+	start := b[:idx]
+	restOfMsg := b[idx+len("\r\n"):]
+
+	parts := strings.Split(start," ")
+	if len(parts) != 3 {
+		return  Error_Bad_Request_Line
+	}
+
+
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	line, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	str := string(line)
+
+	rl, _ ,err := parseRequestLine(str)
 }
